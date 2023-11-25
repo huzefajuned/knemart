3b12149df4c6876497f39350f892b84918117307
Bushra jabin <bushrabintjabin@gmail.com>
2023-11-25 12:26:15 +0530

    Added transition css on button

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -15,7 +15,7 @@ const Products = () => {
                             <h2 className="text-xl pt-5 text-black font-bold tracking-widest">{title}</h2>
                             <h3 className="text-xl">{price}</h3>
                             <h3 className="text-xl tracking-wide">{brand}</h3>
-                            <button className="bg-red-600 w-44 relative ml-14 text-xl mb-3 mt-4 pt-2 rounded-sm text-center flex flex-row  "> <CiShoppingCart className=" text-3xl mt-0 m-2" /> {button} </button>
+                            <button className=" w-44 relative ml-14 text-xl mb-3 mt-4 pt-2 rounded-sm text-center flex flex-row  transition ease-in-out delay-150 bg-red-700 hover:-translate-y-1 hover:scale-110 hover:bg-green-500 duration-300 "> <CiShoppingCart className=" text-3xl mt-0 m-2" /> {button} </button>
                         </div>
                     )
                 })
