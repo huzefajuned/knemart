6e4aeb31c0b0c63ecd46ea4d57260363ac43dad2
Bushra jabin <bushrabintjabin@gmail.com>
2023-11-25 17:13:46 +0530

    remove color from button And added box shadow

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -3,7 +3,7 @@ import { CiShoppingCart } from "react-icons/ci";
 
 const CustomButton = ({button}) => {
     return (
-        <button className=" w-44 relative ml-14 text-xl mb-3 mt-4 pt-2 rounded-sm text-center flex flex-row  transition ease-in-out delay-150 bg-red-700 hover:-translate-y-1 hover:scale-110 hover:bg-green-500 duration-300 "> <CiShoppingCart className=" text-3xl mt-0 m-2" /> {button} </button>
+        <button className=" w-44 relative ml-14 text-xl mb-3 mt-4 pt-2 shadow-md rounded-sm text-center flex flex-row  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:duration-300 "> <CiShoppingCart className=" text-3xl mt-0 m-2" /> {button} </button>
     )
 }
 
