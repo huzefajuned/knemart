3728c7d1ec7b641d0af84e683ca4446eb866e28c
Bushra jabin <bushrabintjabin@gmail.com>
2023-11-26 11:59:02 +0530

    image save on assets folfer

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const loaderImage="/Public/loader.gif"
+const loaderImage="./src/assets/loader.gif"
 const Loader = () => {
   return (
     <div className=" bg-white h-screen w-screen absolute top-0 right-0 flex flex-col  justify-center items-center text-center text-4xl">
