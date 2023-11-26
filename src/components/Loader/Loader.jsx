9d85bb4b2ba47e73453b6780aa93daa05e3f0414
Bushra jabin <bushrabintjabin@gmail.com>
2023-11-26 09:44:15 +0530

    Added loader image and fixe product iamges

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
+const loaderImage="/Public/loadingImage.png"
 const Loader = () => {
   return (
     <div className="bg-blue-300 h-screen w-screen flex flex-col  justify-center items-center text-center text-4xl">
-      please wait ....
+     <img src={loaderImage} alt="" />
     </div>
   );
 };
