4d1e0a140a59a0bd3fd863634655f97a2cd3e2ac
Enayat789 <enayatbhai2006@gmail.com>
2023-11-28 19:06:39 +0530

    data added in dropdown

---
+++ src/components/Dropdown/data.js
@@ -0,0 +1,32 @@
+export const options = [
+  {
+    id: "1",
+    value: "phone",
+    category: "smartphones",
+  },
+  {
+    id: "2",
+    value: "laptop",
+    category: "computers",
+  },
+  {
+    id: "3",
+    value: "tablet",
+    category: "electronics",
+  },
+  {
+    id: "4",
+    value: "camera",
+    category: "photography",
+  },
+  {
+    id: "5",
+    value: "headphones",
+    category: "audio",
+  },
+  {
+    id: "6",
+    value: "smartwatch",
+    category: "wearables",
+  },
+];
